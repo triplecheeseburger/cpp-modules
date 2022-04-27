dfc9ae3f865c7fe2a2da5ce01b15aa96a960d7e6
Han gyul Kim <hakim@c6r9s7.42seoul.kr>
2022-04-27 17:55:00 +0900

    init

--- module01/ex00/Zombie.cpp
+++ module01/ex00/Zombie.cpp
@@ -10,4 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
 
+void	Zombie::announce()
+{
+	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
--- module01/ex00/Zombie.hpp
+++ module01/ex00/Zombie.hpp
@@ -10,4 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIE_H_
+# define ZOMBIE_H_
 
+# include <iostream>
+
+class Zombie {
+	private:
+		std::string		name;
+
+	public:
+		void			announce();
+		static Zombie*	newZombie(std::string name);
+		static void 	randomChump(std::string name);
+		Zombie();
+		~Zombie();
+};
+
+#endif
