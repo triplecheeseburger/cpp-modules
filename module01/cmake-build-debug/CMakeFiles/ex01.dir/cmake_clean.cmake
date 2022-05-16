39596e49a5bb61b872563bf5a3adc9100f14e2fd
triplecheeseburger <hangyulkim94@gmail.com>
2022-05-16 13:18:45 +0900

    re

---
+++ module01/cmake-build-debug/CMakeFiles/ex01.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex01.dir/ex01/Zombie.cpp.o"
+  "CMakeFiles/ex01.dir/ex01/Zombie.cpp.o.d"
+  "CMakeFiles/ex01.dir/ex01/main.cpp.o"
+  "CMakeFiles/ex01.dir/ex01/main.cpp.o.d"
+  "CMakeFiles/ex01.dir/ex01/zombieHorde.cpp.o"
+  "CMakeFiles/ex01.dir/ex01/zombieHorde.cpp.o.d"
+  "ex01"
+  "ex01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
