48cb4bc12cf10fe80a82e756e9423e6954e60c1b
Han gyul Kim <hakim@c6r9s7.42seoul.kr>
2022-05-17 17:12:37 +0900

    to_string was for c++11

--- module01/ex01/zombieHorde.cpp
+++ module01/ex01/zombieHorde.cpp
@@ -17,6 +17,6 @@ Zombie*	Zombie::zombieHorde(int N, std::string name)
 	Zombie*	zombie_horde = new Zombie[N];
 
 	for (int i = 0; i < N; i++)
-		zombie_horde[i].setName(name + std::to_string(i + 1));
+		zombie_horde[i].setName(name);
 	return zombie_horde;
 }
