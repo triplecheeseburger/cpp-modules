2629a1e4cfe35cfe4669ed01caf0c82e7b64dd5f
triplecheeseburger <hangyulkim94@gmail.com>
2022-07-18 21:04:47 +0900

    ex02 main added

--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -13,7 +13,58 @@
 #include "Array.hpp"
 #include <iostream>
 
-int main(void) {
+#define MAX_VAL 750
+int givenMain(void)
+{
+	Array<int> numbers(MAX_VAL);
+	int* mirror = new int[MAX_VAL];
+	srand(time(NULL));
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		const int value = rand();
+		numbers[i] = value;
+		mirror[i] = value;
+	}
+	//SCOPE
+	{
+		Array<int> tmp = numbers;
+		Array<int> test(tmp);
+	}
+
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		if (mirror[i] != numbers[i])
+		{
+			std::cerr << "didn't save the same value!!" << std::endl;
+			return 1;
+		}
+	}
+	try
+	{
+		numbers[-2] = 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		numbers[MAX_VAL] = 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		numbers[i] = rand();
+	}
+	delete [] mirror;//
+	return 0;
+}
+
+int myMain(void) {
 	Array<int> intArr(10);
 	Array<std::string> stringArr(5);
 
@@ -67,4 +118,11 @@ int main(void) {
 	} catch (std::exception & e) {
 		std::cerr << e.what() << std::endl;
 	}
+	return 0;
+}
+
+int main(void) {
+	givenMain();
+	myMain();
+	return 0;
 }
